64648aa722607905868e85d3c3f15be88af74e11
Rupanshu <mail2rupanshu@gmail.com>
2021-06-20 18:40:55 +0530

    added ping url

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const userController = require('../../../controllers/api/v1/user_controller');
 
 router.post('/register',userController.register);
-router.post('/getToken',userController.getToken);
+router.post('/gettoken',userController.getToken);
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const router = express.Router();
-//const homeController = require('../controllers/home_controller');
 
 console.log('router loaded');
 
-//route for home page
-//router.get('/',homeController.home);
-//route user
-// router.use('/users',require('./users'));
-//route for todo
-//router.use('/todo',require('./todo'));
 
 router.use('/api', require('./api'));
+
+router.get("/ping", (req, res) => {
+    return res.status(200).json({
+      msg: "Pong !!! You are currently pingging Blog backend api hosted on heroku ",
+      github:" https://github.com/github-rupanshu/blog_app"
+    });
+  });
+  
 
 module.exports = router;
